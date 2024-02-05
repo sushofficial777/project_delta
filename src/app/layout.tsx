32a86e5f9bc9d71ec538e2945479361c0d90f6d7
sushOfficials <sushofficials44@gmail.com>
2024-02-05 19:19:31 +0530

    fff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
 
+        <link href="https://fonts.googleapis.com/css2?family=Protest+Guerrilla&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Six+Caps&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@700&display=swap" rel="stylesheet" />
+
       </head>
       <body className={inter.className}>
         <BrandTitle />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
 
   return (
     <>
-    <OverLay/>
+    {/* <OverLay/> */}
       <div
       style={{
         display: 'none'
